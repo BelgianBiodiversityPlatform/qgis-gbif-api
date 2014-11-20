50acb1ce0d93567c7fb829c79ec0ef5a8dbef5ce
Nicolas Noé <nicolas@niconoe.org>
2014-11-20 16:29:34 +0100

    Deleted file that was present by mistake.

--- qgis_occurrences_dialog_base.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'qgis_occurrences_dialog_base.ui'
-#
-# Created: Thu Nov 20 14:29:14 2014
-#      by: PyQt4 UI code generator 4.10.4
-#
-# WARNING! All changes made in this file will be lost!
-

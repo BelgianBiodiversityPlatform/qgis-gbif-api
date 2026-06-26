3e8cc055cbf01939f566b9f69ca20102b016ad61
Jules Grillot <jules.grillot@fdn.fr>
2026-06-26 15:37:07 +0200

    tested on QGIS3.16

--- helpers.py
+++ helpers.py
@@ -1,13 +1,14 @@
 import json
 
 from qgis.core import (
+    Qgis,
     QgsVectorLayer,
     QgsFeature,
     QgsGeometry,
     QgsPointXY,
     QgsField,
 )
-from qgis.PyQt.QtCore import QMetaType
+from qgis.PyQt.QtCore import QMetaType, QVariant
 
 
 def create_and_add_layer(project, name, epsg_id=4326):
@@ -63,7 +64,10 @@ def add_gbif_occ_to_layer(occurrences, layer):
             # Add a layer attribute for each JSON fields
             # (if not already encountered)
             if field_index == -1:
-                dp.addAttributes([QgsField(k, QMetaType.Type(10))])
+                if Qgis.QGIS_VERSION_INT >= 33800:
+                    dp.addAttributes([QgsField(k, QMetaType.Type(10))])
+                else:
+                    dp.addAttributes([QgsField(k, QVariant.String)])
 
             attrs.append({"attr": k, "val": _get_field_value(o, k)})
 
